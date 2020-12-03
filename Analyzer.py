60e0783c098e37d372c8f1c886b3917032495d25
sqwu547 <74615906+sqwu547@users.noreply.github.com>
2020-12-02 19:27:03 -0500

    Update Analyzer.py
    plot chart single stock information

--- Analyzer.py
+++ Analyzer.py
@@ -11,6 +11,7 @@ import helper
 from get_all_tickers import get_tickers as gt
 import talib
 # pip install talib
+from matplotlib.pylab import date2num
 
 class Analyzer:
     def __init__(self, tickers, start, end=None):
@@ -276,7 +277,71 @@ class Analyzer:
         plt.ylabel("Return")
         plt.show()
         '''
+    def plot_chart(self, data, n, ticker):
         
+        ''' plot the information for one stock (inclding: Moving Average, MACD, RSI, Volume)
+            param data: datafram for the stock
+            param n: n index before the end date
+            param ticker: single stock ticker
+        '''
+    
+        data["macd"], data["macd_signal"], data["macd_hist"] = talib.MACD(data['Adj Close'])
+        
+        # Get MA10 and MA30
+        data["ma10"] = talib.MA(data["Adj Close"], timeperiod=10)
+        data["ma30"] = talib.MA(data["Adj Close"], timeperiod=30)
+        
+        # Get RSI
+        data["rsi"] = talib.RSI(data["Adj Close"])
+        # Filter number of observations to plot
+        data = data.iloc[-n:]
+        
+        # Create figure and set axes for subplots
+        fig = plt.figure()
+        fig.set_size_inches((20, 16))
+        ax_candle = fig.add_axes((0, 0.72, 1, 0.32))
+        ax_macd = fig.add_axes((0, 0.48, 1, 0.2), sharex=ax_candle)
+        ax_rsi = fig.add_axes((0, 0.24, 1, 0.2), sharex=ax_candle)
+        ax_vol = fig.add_axes((0, 0, 1, 0.2), sharex=ax_candle)
+        
+        # Format x-axis ticks as dates
+        ax_candle.xaxis_date()
+        
+        # Get nested list of date, open, high, low and close prices
+        ohlc = []
+        for date, row in data.iterrows():
+            openp, highp, lowp, closep = row[:4]
+            ohlc.append([date2num(date), openp, highp, lowp, closep])
+     
+        # Plot candlestick chart
+        ax_candle.plot(data.index, data["ma10"], label="MA10")
+        ax_candle.plot(data.index, data["ma30"], label="MA30")
+        # candlestick_ohlc(ax_candle, ohlc, colorup="g", colordown="r", width=0.8)
+        # ax_candle.legend()
+        ax_candle.plot(data.index, data['Adj Close'], label = 'Price')
+        
+        # Plot MACD
+        ax_macd.plot(data.index, data["macd"], label="macd")
+        ax_macd.bar(data.index, data["macd_hist"] * 3, label="hist")
+        ax_macd.plot(data.index, data["macd_signal"], label="signal")
+        ax_macd.legend()
+        
+        # Plot RSI
+        # Above 70% = overbought, below 30% = oversold
+        ax_rsi.set_ylabel("(%)")
+        ax_rsi.plot(data.index, [70] * len(data.index), label="overbought")
+        ax_rsi.plot(data.index, [30] * len(data.index), label="oversold")
+        ax_rsi.plot(data.index, data["rsi"], label="rsi")
+        ax_rsi.legend()
+        
+        # Show volume in millions
+        ax_vol.bar(data.index, data["Volume"] / 1000000)
+        ax_vol.set_ylabel("(Million)")
+       
+        # Save the chart as PNG
+        fig.savefig("charts/" + ticker + ".png", bbox_inches="tight")
+        
+        plt.show()        
 if __name__ == "__main__" : 
      #tickers = ['AAPL', 'MSFT', 'AMZN', 'FB', 'GOOGL', 'GOOG', 'BRK-B', 'JNJ', 'JPM', 'BILI', 'TSLA']
      tickers = gt.get_biggest_n_tickers(40)
@@ -304,7 +369,8 @@ if __name__ == "__main__" :
      
      #test holding period returns
      #print(b.t_test("2020-09-01", 20, w1))
-
+     df =  helper.get_historical_data('AAPL', '2010-01-01','2010-01-01')  
+     b.plot_chart(df, 180, 'AAPL')
 
      #b.plot_holding("2020-09-01", 70, w1)
      
